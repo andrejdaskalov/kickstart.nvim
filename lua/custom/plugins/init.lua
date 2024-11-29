8d02d8339b079cd21aef99ed6f995ef799d5703b
Andrej Daskalov <andrej.daskalov@gmail.com>
2024-11-29 14:38:16 +0100

    added smooth scrolling

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'declancm/cinnamon.nvim',
+    version = '*', -- use latest release
+    -- change default options here
+    config = function()
+      require('cinnamon').setup {
+        -- Enable all provided keymaps
+        keymaps = {
+          basic = true,
+          extra = true,
+        },
+        -- Only scroll the window
+        options = { mode = 'window' },
+      }
+    end,
+  },
+}
